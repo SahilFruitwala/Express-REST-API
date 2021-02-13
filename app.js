72b9194c49100623bc7b9a9b3209d4255ef28db4
SahilFruitwala <sahilfruitwala007@gmail.com>
2021-02-12 21:26:19 -0400

    Done basic CRUD with pagination

--- app.js
+++ app.js
@@ -53,13 +53,6 @@ app.use((req, res, next) => {
   );
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-  // );
-  // res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  // next();
 });
 
 // ? Routes
